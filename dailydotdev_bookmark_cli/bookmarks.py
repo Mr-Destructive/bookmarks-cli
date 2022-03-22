bfd5673069b1a59c3cf56990397bd27e2d6b8c68
Mr-Destructive <gormeet711@gmail.com>
2022-03-22 15:40:03 +0530

    fix and cleanup

--- dailydotdev_bookmark_cli/bookmarks.py
+++ dailydotdev_bookmark_cli/bookmarks.py
@@ -6,6 +6,9 @@ import os.path
 def trim_id(bookmark_id):
     return bookmark_id.split('/')[-1]
 
+def get_feed(url):
+    return feedparser.parse(url)
+
 def get_id():
 
     id_file = '.dailydevid.txt'
@@ -18,6 +21,13 @@ def get_id():
         bookmark_id = input()
         bookmark_id = trim_id(bookmark_id)
 
+        url = f"https://api.daily.dev/rss/b/{bookmark_id}"
+        feed = get_feed(url)
+
+        if 'feed' in feed:
+            with open(id_file, 'w') as file:
+                file.write(bookmark_id)
+
     return bookmark_id
 
 
@@ -28,11 +38,7 @@ def get_bookmarks_list():
 
     if daily_id:
         url = f"https://api.daily.dev/rss/b/{daily_id}"
-        feed = feedparser.parse(url)
-
-        if 'feed' in feed:
-            with open(id_file, 'w') as file:
-                file.write(daily_id)
+        feed = get_feed(url)
 
         bookmarks = feed.entries
         return bookmarks
@@ -43,21 +49,23 @@ def get_bookmarks_list():
 
 def get_bookmarks():
 
-        print(f"[red]{name}")
+    bookmarks = get_bookmarks_list()
 
-        bookmarks = get_bookmarks_list()
+    name = get_feed().feed.title
+    print(f"[red]{name}")
 
-        for bookmark in bookmarks:
-            link = bookmark.link.split("?utm_source")[0].strip()
-            
-            blog_base_link = link.split("/posts/")[0].strip().replace("app", "api") 
-            
-            blog_id = link.split("/posts/")[1].strip()
-            
-            blog_link = blog_base_link+"/r/"+blog_id
+    for bookmark in bookmarks:
+        link = bookmark.link.split("?utm_source")[0].strip()
+        
+        blog_base_link = link.split("/posts/")[0].strip().replace("app", "api") 
+        
+        blog_id = link.split("/posts/")[1].strip()
+        
+        blog_link = blog_base_link+"/r/"+blog_id
 
-            print(f""" - [yellow][link={blog_link}]{bookmark.title}[/link][/yellow]\n
-    [blue][link={blog_link}]{blog_link}[/link]\n""")
+        print(f""" - [yellow][link={blog_link}]{bookmark.title}[/link][/yellow]\n
+[blue][link={blog_link}]{blog_link}[/link]\n""")
 
-
+        if len(bookmarks) == 0:
+            print("[yellow]Sorry, you do not have any bookmarks created in your daily.dev account feed!")
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.7'
+VERSION = '0.1.73'
 PACKAGE_NAME = 'dailydotdev_bookmark_cli'
 AUTHOR = 'Meet Gor'
 AUTHOR_EMAIL = 'gormeet711@gmail.com'
